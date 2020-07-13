37b7427f9cc972bc21604dac2b53d12ba87ab163
Grygorii <shevchenko.grygorii@gmail.com>
2020-07-13 11:32:38 +0300

    fix login

--- src/modules/user/Login.ts
+++ src/modules/user/Login.ts
@@ -1,14 +1,14 @@
-import bcrypt from "bcryptjs";
-import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
-import { User } from "../../entity/User";
-import { MyContext } from "../../types/MyContext";
+import bcrypt from 'bcryptjs';
+import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
+import { User } from '../../entity/User';
+import { MyContext } from '../../types/MyContext';
 
 @Resolver()
 export class LoginResolver {
   @Mutation(() => User, { nullable: true })
   async login(
-    @Arg("email") email: string,
-    @Arg("password") password: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string,
     @Ctx() ctx: MyContext
   ): Promise<User | null> {
     const user = await User.findOne({ where: { email } });
@@ -20,10 +20,6 @@ export class LoginResolver {
     const valid = await bcrypt.compare(password, user.password);
 
     if (!valid) {
-      return null;
-    }
-
-    if (!user.confirmed) {
       return null;
     }
 
